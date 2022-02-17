25491bb76d75fcd9d0833d34a2cd826672875b1a
mrYadav2079 <89142226+mrYadav2079@users.noreply.github.com>
2022-02-17 18:05:31 +0530

    Create singleResponsibility.java

---
+++ S/singleResponsibility.java
@@ -0,0 +1,25 @@
+class Book{
+    private String name;
+    private String author;
+    private String text;
+
+    public String replaceWordInText(String word){
+        return text.replaceAll(word,text);
+    }
+
+    public boolean isWordInText(String word){
+        return text.contains(word);
+    }
+}
+class BookPrinter{
+    void PrintTextToConsole(String text){
+        System.out.println(text);
+    }
+}
+public class SingleResponsibility {
+    public static void main(String args[]){
+         //According to single responsibility every class should perform single functionality
+        //Implementation of multiple functionality violate the rule
+
+    }
+}
